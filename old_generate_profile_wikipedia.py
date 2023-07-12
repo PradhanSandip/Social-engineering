7a12a1fb0529eee38ce9b484059f87387e9db052
mrduck6 <mrduckandduck6@gmail.com>
2023-07-12 21:59:14 +0100

    complete profile data

--- old_generate_profile_wikipedia.py
+++ old_generate_profile_wikipedia.py
@@ -14,29 +14,6 @@ wiki = wikipediaapi.Wikipedia(f'CelebInfo/0 {os.environ.get("contact")}', "en")
 #json file to save celeb info
 save_file = "celeb-data.json"
 
-'''read and retun json file'''
-def read_json(file_name):
-    #if file does not exist
-    if(not Path(save_file).exists()):
-        #create file
-        with open(file_name, "w") as file:
-            #write data
-            file.write("{}")
-    #open file
-    with open(file_name, "r+") as file:
-        #return json data
-        return json.load(file)
-
-'''function to update and save json file'''
-def update_json(file_name, content):
-    #load json data of a file
-    data = read_json(file_name)
-    #update json
-    data.update(content)
-    #open json file
-    with open(file_name, "w") as file:
-        #save updated json 
-        json.dump(data, file)
 
 #remove emojis from the string
 def remove_emojis(data):
@@ -63,126 +40,100 @@ def remove_emojis(data):
     return re.sub(emoj, '', data)
 
 
-
-
-
 #return url of page matches name
 def get_urls(name):
-    #search wiki with name
     search = wikipedia.search(name)
-    #if search returns result
     if(len(search) > 0):
-        #list to store urls
         urls = []
-        #for each result
         for r in search:
-            #save the full url
             urls.append(wiki.page(r).fullurl)
-    #return urls
     return urls
 
-#parse url to extract data
-def parse_url(url, username):
-    #open csv file to retrieve tweeet data 
-    dataFrame = pd.read_csv("extra-data.csv", sep=":::",engine='python')
-    #get row that matches username
-    data = dataFrame[dataFrame["username"] == username]
-    #list of keywords from twitter profile
-    keywords = [i for i in data.values]
-    #search keywords
-    req = requests.get(url)
-    #TODO: parse html
+# #parse url to extract data
+# def parse_url(url, username):
+#     #open csv file to retrieve data
+#     dataFrame = pd.read_csv("extra-data.csv", sep=":::",engine='python')
+#     data = dataFrame[dataFrame["username"] == username]
+#     keywords = [i for i in data.values]
+#     #search keywords
+#     req = requests.get(url)
 
 #simple search check if the wikipedia page exist with the name, if exist do keyword search otherwise search all the result
 def simple_search(name,username):
-    #print current username and name
+    #print current name and username
     print(name,username)
-    #score based on keyword match in wiki
+    #score based on keyword matches
     score = 0
     #get wiki page with name
     result = wiki.page(name)
-    #if result exists
+    #if page exist
     if(result.exists() and result.summary != ""):
-        #add score
+        #add score 
         score += 1
-        #get full page url
+        #get page full url
         url = result.fullurl
-        #get html 
+        #get html
         req = requests.get(url)
-        #if request is ok
+        #if status ok
         if(req.status_code == 200):
-            #open csv file to retrieve user profile data
+            #open csv file containing tweets
             dataFrame = pd.read_csv("extra-data.csv", sep=":::",engine='python')
-            #get userprofile that matches username
+            #get the row matching username
             data = dataFrame[dataFrame["username"] == username]
             #BS4 object
             soup = BeautifulSoup(req.content, 'html.parser')
-            #for each column in profile
+            #for each column in csv
             for keys in data.values.tolist():
-                #for each word in column
+                #for each word
                 for key in keys:
-                    #remove special characters and emojis
-                    for keyword in re.sub(r"[+(),*]", " ", remove_emojis(str(key))).split(" "):
-                        #if word not empty
+                    #remove specia characters and emojis and create list of words
+                    for keyword in re.sub(r"[+(),*?-]", " ", remove_emojis(str(key))).split(" "):
+                        #if the word is not empty
                         if(keyword != ""):
-                            #print keyword
+                            #print key word
                             print("Keyword: "+keyword)
-                            #if keyword is found
+                            #if keyword in wiki page 
                             if(len(soup.body.findAll(string=re.compile(str(keyword)+"|"+str(keyword.lower())))) > 0):
                                 #add score
                                 score += 1
-    #if score 1 or more
+    #if score more than 1
     if(score > 1):
-        #find the infobox in wiki page
+        
         table_row = soup.find(class_="infobox")
-        #if infobox does not exist
         if(table_row == None):
-            #exit
             return
-        #get all the row in infobox
         table_row = soup.find(class_="infobox").find_all("tr")
-        #initial json object
         json_object = {username:{"name":name,"full-url":url}}
-        #for each row in infobox
         for row in table_row:
-            #empty result
             content = ""
-            #find the infobox head (key/title/column name)
             table_head = row.find("th")
-            #if table head exist
             if(table_head != None):
-                #get the text
                 key = table_head.text
-                #find table data
                 parent = row.find("td")
-                #if table data exist 
                 if(parent != None):
-                    #for all the table data in a row
                     for child in parent.children:
-                        #if the child is not style tag
                         if(child.name != "style"):
-                            #if child is div and contain ul element
                             if(child.name == "div" and child.find("ul") != None):
-                                #get all the li elements
                                 for index,li in enumerate(child.find_all("li")):
-                                    #as long as the li element no the end li
                                     if(index != len(child.find_all("li"))-1):
-                                        #add the text to the content + a seperator "/"
                                         content += li.text + "/"
-                                    #else just add the text
                                     else:
                                         content += li.text
-                            #if the child is not div or contains ul element
                             else:
-                                #just get the text
                                 content += child.text
-                    #create a json object with username as key and content as value            
                     json_object[username][" ".join(table_head.text.splitlines()).replace(u"\u2013",u"-").replace(u'\xa0', u' ')] = " ".join(content.replace(u'\xa0', u' ').replace(u"\u2013",u"-").replace("\n"," ").replace(u'\\u',u' ').encode('ascii',errors='ignore').decode().splitlines())
-        #update the json file with new key value
-        update_json(save_file, json_object)
-        #print the new json object
+                    # print(table_head.text+"->")
+                    # print(content.strip())  
+        if(not Path(save_file).exists()):
+            with open(save_file, "w") as jf:
+                json.dump(json_object, jf)  
+        else:
+            with open(save_file, "r") as jf:
+                data = json.load(jf)
+            data.update(json_object)
+            with open(save_file, "w") as jf:
+                json.dump(data, jf) 
         print(json_object)
-        #sleep for 5 sec to avoid server timeout
         time.sleep(5)       
             
 
@@ -190,19 +141,24 @@ def simple_search(name,username):
         #do query url search    
         pass    
 
-#get all the tweeter profile
+#
 celeb_list = pd.read_csv("extra-data.csv", sep=":::",engine="python")
-#for each profile
+ignore = []
+with open("ignore_missing.json", "r") as file:
+    ignore = json.load(file)["data"]["ignore"]
 for celeb in celeb_list.to_records():
-    #load the json file containing celeb data
-    data = read_json(save_file)
-    #if the json file does not contain celebrity with username
-    if(celeb["username"] not in data.keys()):
-        #retrieve data from wiki with name
-        simple_search(celeb["name"],celeb["username"])
-        #sleep for 1 sec
-        time.sleep(1)
-    
+   if(celeb["username"] not in ignore):
+        if(Path(save_file).exists()):
+            with open(save_file, "r") as jf:
+                data = json.load(jf)
+            if(celeb["username"] not in data.keys()):
+                simple_search(celeb["name"],celeb["username"])
+                
+            else:
+                continue
+        else:
+            simple_search(celeb["name"],celeb["username"])
+        
 
 
 
